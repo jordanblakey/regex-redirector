f39d674b9a88641c68fc192fb159978d85eaf0b6
Jordan <454820+jordanblakey@users.noreply.github.com>
2022-12-10 12:40:48 -0700

    Util funcs to save redirects in chrome.storage

--- popup/popup.js
+++ popup/popup.js
@@ -1,1 +1,17 @@
-console.log('This is a popup!')
+async function putRedirect(pattern, url) {
+  await chrome.storage.local.get(["redirects"]).then(res => {
+    if (!res.hasOwnProperty("redirects")) res.redirects = {}
+    res.redirects[pattern] = url
+    chrome.storage.local.set({"redirects": res.redirects})
+  })
+}
+
+async function deleteRedirect(pattern) {
+  await chrome.storage.local.get(["redirects"]).then(res => {
+    if (res.hasOwnProperty("redirects")) {
+      delete res.redirects[pattern]
+      chrome.storage.local.set({"redirects": res.redirects})
+    }
+  })
+}
+
