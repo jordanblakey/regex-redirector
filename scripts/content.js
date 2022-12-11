8003b073804b0177357aee52d8d0c487439a08fb
Jordan <454820+jordanblakey@users.noreply.github.com>
2022-12-10 17:41:40 -0700

    Adds initial URL pattern check.

--- scripts/content.js
+++ scripts/content.js
@@ -1,20 +1,24 @@
 const logStyle =
   "padding: 6px 12px; border-radius: 20px; font-weight: bold; color: white; background-color: #4158D0; background-image: linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%);";
 
+function checkRedirects() {
+  chrome.storage.local.get(["redirects"]).then((response) => {
+    for (pString in response.redirects) {
+      let pattern = new RegExp(pString.slice(1, pString.length - 1));
+      if (location.href.match(pattern)) {
+        chrome.runtime.sendMessage({ redirectNotification: true });
+        location.href = `http://${response.redirects[pString]}`;
+      }
+    }
+  });
+}
+
 function createObserver() {
   var previousUrl = "";
   var observer = new MutationObserver((mutations) => {
     if (location.href !== previousUrl) {
       previousUrl = location.href;
-      chrome.storage.local.get(["redirects"]).then((response) => {
-        for (pString in response.redirects) {
-          let pattern = new RegExp(pString.slice(1, pString.length - 1));
-          if (location.href.match(pattern)) {
-            chrome.runtime.sendMessage({ redirectNotification: true });
-            location.href = `http://${response.redirects[pString]}`;
-          }
-        }
-      });
+      checkRedirects();
     }
   });
   const config = { subtree: true, childList: true };
@@ -22,4 +26,5 @@ function createObserver() {
   console.log("%c▶️▶️ Regex Redirector is active.", logStyle);
 }
 
+checkRedirects(); // initial check
 createObserver();
